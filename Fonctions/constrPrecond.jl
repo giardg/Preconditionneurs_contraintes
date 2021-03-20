d87cbf6ee16a34e7debb87a10ccbd42c6184a56a
giardg <gregoryg97@hotmail.com>
2021-03-20 19:11:27 -0400

    Ajout du N et vérification pour un M non symétrique

--- Fonctions/constrPrecond.jl
+++ Fonctions/constrPrecond.jl
@@ -10,15 +10,15 @@ function blocGJacobi(M)
     return G⁻¹
 end
 
-function constrPrecond(G⁻¹,A)
+function constrPrecond(G⁻¹,A,N)
 
-    #Fonction qui construit P⁻¹ pour un bloc G = diag(M)
+    #Fonction qui construit P⁻¹ (preconditionneur par contrainte) a partir de G⁻¹ et de la matrice A
 
     m, n = size(A)
-    F2 = (A*G⁻¹*(A')) #Possibilite de faire des factorisation incomplete
-    Aᵀ⁺GA⁺ = LinearOperator(Float64, m, m, true, true, u -> F2\u) 
+    F2 = Hermitian(N+A*G⁻¹*(A')) #Possibilite de faire des factorisations incompletes
+    Schur_inv = LinearOperator(Float64, m, m, true, true, u -> F2\u) 
 
-    opM = LinearOperator(Float64, n+m, n+m, true, true, u -> [(G⁻¹-G⁻¹*A'*Aᵀ⁺GA⁺*A*G⁻¹)*u[1:n] + G⁻¹*A'*Aᵀ⁺GA⁺*u[n+1:m+n]; Aᵀ⁺GA⁺*A*G⁻¹*u[1:n]-Aᵀ⁺GA⁺*u[n+1:m+n]]) 
+    opM = LinearOperator(Float64, n+m, n+m, true, true, u -> [(G⁻¹-G⁻¹*A'*Schur_inv*A*G⁻¹)*u[1:n] + G⁻¹*A'*Schur_inv*u[n+1:m+n]; Schur_inv*A*G⁻¹*u[1:n]-Schur_inv*u[n+1:m+n]]) 
 
     return opM
 
--- Fonctions/main.jl
+++ Fonctions/main.jl
@@ -2,23 +2,25 @@ using Krylov
 using SparseArrays
 include("constrPrecond.jl")
 
-A = rand(90,120)
-M = rand(120,120)
-M = M'*M
+A = sprand(90,120,0.5)
+M = sprand(120,120,0.5)
+N = sprand(90,90,0.0)
+M = M'*M #M = M'*M pour hermitien, mais pas necessaire
+N = N'*N;
 
 m,n = size(A)
 
-mat = [M A'; A zeros(m,m)]
+mat = [M A'; A -N]
 b = rand(m+n)
 
 G⁻¹ = blocGJacobi(M)
-opM = constrPrecond(G⁻¹,A)
+opM = constrPrecond(G⁻¹,A,N)
 
-x, stats = dqgmres(mat, b)
+x, stats = cgs(mat, b, itmax = 1000)
 nbiter = length(stats.residuals) - 1
 println("Convergence en $nbiter itérations sans préconditionneur.")
 
-x2, stats = dqgmres(mat, b, M = opM)
+x2, stats = cgs(mat, b, N = opM, itmax = 1000)
 nbiter = length(stats.residuals) - 1
 println("Convergence en $nbiter itérations avec préconditionneur.")
 
