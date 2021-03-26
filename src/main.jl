f36a3bd79a19b83f8157e5226f1d8b53ba2836ce
giardg <gregoryg97@hotmail.com>
2021-03-26 18:16:29 -0400

    Prise de résultats

--- src/main.jl
+++ src/main.jl
@@ -7,7 +7,7 @@ include("FonctionsConstraintPrecond.jl")
 A = sprand(90,120,1.0)
 N = sprand(90,90,0.0) #N = 0 souvent, mais pas nécessaire
 M = sprand(120,120,1.0)
-M = M'*M #M = M'*M pour hermitien, mais pas nécessaire (pour gmres oui)
+M = M #M = M'*M pour hermitien, mais pas nécessaire (pour gmres oui)
 N = N'*N;
 
 m,n = size(A)
@@ -26,16 +26,16 @@ time3 = zeros(kmax)
 time4 = zeros(kmax)
 
 for k = 1:kmax
-    t1 = @elapsed x, stats = solvePrecond(M,A,N,b, "gmres", false, "I", k);
+    t1 = @elapsed x, stats = solvePrecond(M,A,N,b, "cgs", false, "I", k);
     res1[k] = norm(b-mat*x)
     time1[k] = t1
-    t2 = @elapsed x2, stats = solvePrecond(M,A,N,b, "gmres", true, "I", k);
+    t2 = @elapsed x2, stats = solvePrecond(M,A,N,b, "cgs", true, "I", k);
     res2[k] = norm(b-mat*x2)
     time2[k] = t2
-    t3 = @elapsed x3, stats = solvePrecond(M,A,N,b, "gmres", true, "Diagonal", k);
+    t3 = @elapsed x3, stats = solvePrecond(M,A,N,b, "cgs", true, "Diagonal", k);
     res3[k] = norm(b-mat*x3)
     time3[k] = t3
-    t4 = @elapsed x4, stats = solvePrecond(M,A,N,b, "gmres", true, "LLDL", k);
+    t4 = @elapsed x4, stats = solvePrecond(M,A,N,b, "cgs", true, "LLDL", k);
     res4[k] = norm(b-mat*x4)
     time4[k] = t4
 end
@@ -45,11 +45,11 @@ plot_ref1=plot!(1:kmax, res2, color=2, label="G = I")
 plot_ref1=plot!(1:kmax, res3, color=3, label="G = Diagonal(M)")
 plot_ref1=plot!(1:kmax, res4, yaxis=:log10, xlabel=L"k", color=4, ylabel=L"\|r_k\|", label="G = LLDL",legend=:best, fmt = :png)
 
-savefig(plot_ref1,"C:\\Users\\Gregory.Giard\\Maitrise\\Session 2\\Algebre lineaire numerique appliquee\\Projet\\Preconditionneurs_contraintes\\gmres_rk")
+#savefig(plot_ref1,"C:\\Users\\Gregory.Giard\\Maitrise\\Session 2\\Algebre lineaire numerique appliquee\\Projet\\Preconditionneurs_contraintes\\cgs_rk_nonHerm")
 
 plot_ref2=plot(1:kmax, time1, color=1, label="Sans préconditionneur")
 plot_ref2=plot!(1:kmax, time2, color=2, label="G = I")
 plot_ref2=plot!(1:kmax, time3, color=3, label="G = Diagonal(M)")
 plot_ref2=plot!(1:kmax, time4, yaxis=:log10, xlabel=L"k", color=4, ylabel="Temps (s)", label="G = LLDL",legend=:bottomright, fmt = :png)
 
-savefig(plot_ref2,"C:\\Users\\Gregory.Giard\\Maitrise\\Session 2\\Algebre lineaire numerique appliquee\\Projet\\Preconditionneurs_contraintes\\gmres_time")
+#savefig(plot_ref2,"C:\\Users\\Gregory.Giard\\Maitrise\\Session 2\\Algebre lineaire numerique appliquee\\Projet\\Preconditionneurs_contraintes\\cgs_time_nonHerm")
